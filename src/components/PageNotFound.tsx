49555075487eab9f79a4194cc2d737b6878c0ab0
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-05-19 13:06:11 +0300

    add 404 page

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="not-found-page">
+      <Link to="/">
+        HOME PAGE
+      </Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
